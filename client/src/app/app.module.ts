8b615ab23d9d7d2163d8102c4e8389813d027475
aunzo_mick@mail.com <aunzo_mick@mail.com>
2022-03-10 16:04:05 +0800

    End of Section 4

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 @NgModule({
   declarations: [
     AppComponent
+ 
   ],
   imports: [
     BrowserModule,
